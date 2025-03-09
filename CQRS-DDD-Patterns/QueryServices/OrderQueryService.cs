c7b225d64118eb25a14d5cc0013040eae77bdbb1
Maciej <48365963+Mchajda@users.noreply.github.com>
2025-03-09 16:15:29 +0100

    Create OrderQueryService.cs

---
+++ CQRS-DDD-Patterns/QueryServices/OrderQueryService.cs
@@ -0,0 +1,20 @@
+﻿using CQRS_DDD_Patterns.Models;
+using CQRS_DDD_Patterns.Repositories;
+
+namespace CQRS_DDD_Patterns.QueryServices
+{
+    public class OrderQueryService
+    {
+        private readonly IOrderRepository _readRepository;
+
+        public OrderQueryService(IOrderRepository readRepository)
+        {
+            _readRepository = readRepository;
+        }
+
+        public async Task<Order> GetOrderById(string orderId)
+        {
+            return await _readRepository.GetByIdAsync(orderId);
+        }
+    }
+}
